8a471f25a784160d2df65db0506e2b7883c76800
Ayush Acharjya <ayushacharjya@gmail.com>
2024-05-09 22:07:19 +0100

    added

--- Question 3075 - Maximize Happiness of Selected Children/src/OptimisedSol.java
+++ Question 3075 - Maximize Happiness of Selected Children/src/OptimisedSol.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class OptimisedSol {
+import java.util.Arrays;
+
+class OptimisedSol
+{
+    public long maximumHappinessSum(int[] happiness, int k)
+    {
+        long ans = 0L;
+        Arrays.sort(happiness);
+
+        int diff = 0, n = happiness.length;
+        for(int i = n-1; i >= n-k; i--)
+        {
+            int val = Math.max(happiness[i] + diff, 0);
+            diff--;
+            ans += val;
+        }
+
+        return ans;
+    }
 }
--- Question 3075 - Maximize Happiness of Selected Children/src/Solution.java
+++ Question 3075 - Maximize Happiness of Selected Children/src/Solution.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution
+{
+    public long maximumHappinessSum(int[] happiness, int k)
+    {
+        long ans = 0L;
+
+        PriorityQueue<Integer> q = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int n : happiness) q.add(n);
+
+        int diff = 0;
+        for(int i = 0; i < k && !q.isEmpty(); i++)
+        {
+            int val = Math.max(q.poll() + diff, 0);
+            diff--;
+            ans += val;
+        }
+
+        return ans;
+    }
 }
